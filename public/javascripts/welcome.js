8d8f00d44c8bc25af5add2186582932dc7b3296b
Stanley <stanleyjsdz@gmail.com>
2015-03-21 19:43:41 -0600

    initial commit. Site structure functional

---
+++ public/javascripts/welcome.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#home').hover(function(){
+    $('#banner').css({ 'color': 'green' });
+  }); 
+});
---
+++ server.rb
@@ -0,0 +1,23 @@
+# gem 'sinatra'
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  erb :welcome
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/services' do
+  erb :services
+end
+
+get '/clients' do
+  erb :clients
+end
+
+get '/contact' do
+  erb :contact
+end
