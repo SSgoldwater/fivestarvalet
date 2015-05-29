ff5531b286b72c2e21255bec0c8b284fceb09b7b
Stanley <stanleyjsdz@gmail.com>
2015-05-29 16:53:55 -0600

    lots of css updating

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-  $('li').click(function(e) {
-    var $this = $(this);
-    if (!$this.hasClass('active')) {
-      $this.addClass('active');
-    }
-  });
+  var url = window.location.href;
+  var tab = url.split('/'); 
+ 
+ $(".nav a").on("click", function(){
+     $(".nav").find(".active").removeClass("active");
+       $(this).parent().addClass("active");
+ }); 
 });
