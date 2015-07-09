766ac8489ecaa173364f356eea5a13504130d4bd
Stanley <stanleyjsdz@gmail.com>
2015-07-08 19:26:57 -0600

    add class to server file

--- app/server.rb
+++ app/server.rb
@@ -1,23 +1,24 @@
 # gem 'sinatra'
 require 'sinatra'
 require "sinatra/reloader" if development?
+class FiveStarApp < Sinatra::Base
+  get '/' do
+    erb :welcome
+  end
 
-get '/' do
-  erb :welcome
-end
+  get '/about' do
+    erb :about
+  end
 
-get '/about' do
-  erb :about
-end
+  get '/services' do
+    erb :services
+  end
 
-get '/services' do
-  erb :services
-end
+  get '/clients' do
+    erb :clients
+  end
 
-get '/clients' do
-  erb :clients
-end
-
-get '/contact' do
-  erb :contact
+  get '/contact' do
+    erb :contact
+  end
 end
